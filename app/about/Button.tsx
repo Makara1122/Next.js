eb6bb80c96020bbf329ef28a53487ab1ae0dd728
Your Name <you@example.com>
2024-03-14 09:36:00 +0700

    Next.js lesson1

---
+++ app/about/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button() {
+  return (
+    <div className='p-4 w-[130px] bg-yellow-500 text-white'>
+      <p>button</p>
+    </div>
+  )
+}
---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "./Button";
+export default function page() {
+    return (
+        <>
+        <div>I love Next.js</div>
+        <Button/>
+        </>
+    )
+}
